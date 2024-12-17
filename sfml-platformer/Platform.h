52fafee0e9bcbbee14bea2af8110ea4a6033698c
OscarLarm <118494885+OscarLarm@users.noreply.github.com>
2024-12-17 15:31:31 +0100

    Add gameobject, platform and level classes. Change game class function loop

---
+++ sfml-platformer/Platform.h
@@ -0,0 +1,16 @@
+#ifndef PLATFORM_H
+#define PLATFORM_H
+#include "GameObject.h"
+
+class Platform : public GameObject
+{
+private:
+
+
+public:
+	Platform();
+	~Platform();
+
+};
+
+#endif // !PLATFORM_H
