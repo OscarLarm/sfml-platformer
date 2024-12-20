9d952a18236e458012363d5ef89572e6b1cb6c95
OscarLarm <118494885+OscarLarm@users.noreply.github.com>
2024-12-20 18:06:26 +0100

    Add sprite and texture for platform.

--- sfml-platformer/Platform.cpp
+++ sfml-platformer/Platform.cpp
@@ -1,9 +1,23 @@
 #include "Platform.h"
 
-Platform::Platform()
+Platform::Platform(sf::Vector2f position)
 {
+	this->texture.loadFromFile("../assets/platform.png");
+	this->sprite.setTexture(this->texture);
+	this->sprite.setTextureRect(sf::IntRect(0, 0, 32, 32));
+	this->position = position;
+	this->sprite.setPosition(this->position);
 }
 
 Platform::~Platform()
+{
+}
+
+void Platform::draw(sf::RenderTarget& target, sf::RenderStates states) const
+{
+	target.draw(this->sprite);
+}
+
+void Platform::setPosition(const sf::Vector2f& position)
 {
 }
